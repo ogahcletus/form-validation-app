b4afd4aaa0897cf75ddc33503031a8fb19309714
cletus ogah <ogahcletus@gmail.com>
2022-08-22 12:20:16 +0100

    added source files & Css

---
+++ src/App.js
@@ -0,0 +1,85 @@
+
+import './App.css';
+import React, {useState} from 'react';
+import InputFields from './Components/InputFields'
+
+
+
+function App() {
+
+  const inputRefs = React.useRef([
+    React.createRef(),
+    React.createRef()
+  ]);
+
+  const [data, setData] = useState({});
+
+  const handleChange = (name, value) => {
+    setData(prevState => ({...prevState, [name]: value
+    }))
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let isValid = true
+
+   for  (let i = 0; i < inputRefs.current.length; i++){
+   const valid = inputRefs.current[i].current.validate();
+
+   console.log(valid);
+
+    if(!valid) {
+      isValid = false
+
+       
+     }
+    
+
+   } 
+   
+   if(!isValid){
+      return
+   }
+    console.log('Logged In')
+   //Otherwise carry on as normal
+
+  }
+
+  
+
+  return (
+    <div className="App">
+      
+     
+      <form className='form' onSubmit={handleSubmit}>
+      <h1>REACT VALIDATION FORM</h1>
+          
+       <InputFields 
+          ref={inputRefs.current[0]}
+          name='username'
+          label='Username'
+          onChange={handleChange}
+          validation={"required|min:6|max:12"}
+       />
+
+        <InputFields 
+          ref={inputRefs.current[1]}
+          name='password'
+          label='Password'
+          onChange={handleChange}
+          validation="required|min:6"
+       />
+          <button type='button'>Login</button>
+
+      <footer className='footer'>
+        <p>&copy; Cletus_Ogah @2022</p>
+     </footer>
+      </form>
+     
+    </div>
+  );
+}
+
+export default App;
